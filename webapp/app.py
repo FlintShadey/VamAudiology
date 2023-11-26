2daa476e5529c0929c529febebcf6f4dd2c367e3
FlintShadey <flintsmith@gmail.com>
2023-11-25 18:12:34 -0600

    this  started to change the success - registration

--- webapp/app.py
+++ webapp/app.py
@@ -1,50 +1,63 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, jsonify, render_template, redirect, url_for, flash, request
 from forms import LoginForm, RegistrationForm
-
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "ec58e0a02e7d66b29d3c063d5a7293b8"
 
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    registration_form = RegistrationForm()  # Add this line to create an instance of RegistrationForm
-    if form.validate_on_submit():
-        # Login logic goes here...
-        flash('You have been logged in!', 'success')
-        return redirect(url_for('landing_page'))  # Make sure this is the function name, not the endpoint
-    return render_template('login.html', title='Login', form=form, registration_form=registration_form)
+    login_form = LoginForm()
+    registration_form = RegistrationForm()
 
-@app.route('/register', methods=['GET', 'POST'])
+
+    if "login_submit" in request.form and login_form.validate_on_submit():
+        # Login logic goes here...
+        flash("You have been logged in!", "success")
+        return redirect(url_for("landing_page"))
+
+    if "register_submit" in request.form and registration_form.validate_on_submit():
+        # Registration logic goes here...
+        flash("Your account has been created! You are now able to log in", "success")
+        return redirect(url_for("login"))
+
+    return render_template(
+        "login.html",
+        title="Login",
+        form=login_form,
+        registration_form=registration_form,
+    )
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Insert logic to register the user here
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))  # Redirect back to the login page
-    # Pass the registration form as a separate variable
-    return render_template('login.html', title='Register', registration_form=form, form=LoginForm())
+        # Registration logic goes here...
+        return jsonify(success=True)
+    else:
+        # Return validation errors
+        return jsonify(success=False, errors=form.errors), 400
 
 
+@app.route("/landing-page")
+def landing_page():
+    return render_template("landing-page.html", title="Home")
 
-@app.route('/landing-page')
-def landing_page(): 
-    return render_template('landing-page.html',  title='Home')
 
-@app.route('/vam-audio-add')
+@app.route("/vam-audio-add")
 def vam_audio_add():
-    return render_template('vam-audio-add.html', title='Add Data')
+    return render_template("vam-audio-add.html", title="Add Data")
 
-@app.route('/vam-audio-download')
+
+@app.route("/vam-audio-download")
 def vam_audio_download():
-    return render_template('vam-audio-download.html', title='Download Data')
+    return render_template("vam-audio-download.html", title="Download Data")
 
-@app.route('/vam-audio-search')
+
+@app.route("/vam-audio-search")
 def vam_audio_search():
-    return render_template('vam-audio-search.html', title='Search Data')
+    return render_template("vam-audio-search.html", title="Search Data")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- webapp/forms.py
+++ webapp/forms.py
@@ -2,6 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+                            validators=[DataRequired(), Email()])
+    password = PasswordField('Password', 
+                            validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    login_submit = SubmitField('Login')
+    
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
                             validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,12 +17,5 @@ class RegistrationForm(FlaskForm):
                             validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"id": "signupPassword"})
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')],  render_kw={"id": "signupPasswordConfirm"})
-    submit = SubmitField('Sign Up')
+    register_submit = SubmitField('Sign Up')
 
-class LoginForm(FlaskForm):
-    email = StringField('Email', 
-                            validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-                            validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')

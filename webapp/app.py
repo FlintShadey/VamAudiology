bb817c63998f9f9630f8efe4b3c3a849c4444487
FlintShadey <flintsmith@gmail.com>
2023-11-28 17:38:36 -0600

    1128

--- webapp/app.py
+++ webapp/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "ec58e0a02e7d66b29d3c063d5a7293b8"
 
 
+
 @app.route("/", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
     registration_form = RegistrationForm()
-
 
     if "login_submit" in request.form and login_form.validate_on_submit():
         # Login logic goes here...
@@ -25,9 +25,10 @@ def login():
     return render_template(
         "login.html",
         title="Login",
-        form=login_form,
+        login_form=login_form,
         registration_form=registration_form,
     )
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
--- webapp/forms.py
+++ webapp/forms.py
@@ -3,13 +3,15 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class LoginForm(FlaskForm):
+    
     email = StringField('Email', 
                             validators=[DataRequired(), Email()])
     password = PasswordField('Password', 
                             validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     login_submit = SubmitField('Login')
-    
+    submit = SubmitField('Login')
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
                             validators=[DataRequired(), Length(min=2, max=20)])

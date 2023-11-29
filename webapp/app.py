813e288ec0d64a9e122ca670bd1b80c7410d10ae
Flint <flintsmith@gmail.com>
2023-11-28 22:08:38 -0600

    1128

--- webapp/app.py
+++ webapp/app.py
@@ -7,6 +7,8 @@ app.config["SECRET_KEY"] = "ec58e0a02e7d66b29d3c063d5a7293b8"
 
 
 
+from flask import jsonify
+
 @app.route("/", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
@@ -14,13 +16,11 @@ def login():
 
     if "login_submit" in request.form and login_form.validate_on_submit():
         # Login logic goes here...
-        flash("You have been logged in!", "success")
-        return redirect(url_for("landing_page"))
+        return jsonify(success=True)
 
     if "register_submit" in request.form and registration_form.validate_on_submit():
         # Registration logic goes here...
-        flash("Your account has been created! You are now able to log in", "success")
-        return redirect(url_for("login"))
+        return jsonify(success=True)
 
     return render_template(
         "login.html",
@@ -40,7 +40,7 @@ def register():
         return jsonify(success=False, errors=form.errors), 400
 
 
-@app.route("/landing-page")
+@app.route("/landing_page")
 def landing_page():
     # Check if 'registered' parameter is in the query string
     if 'registered' in request.args:
